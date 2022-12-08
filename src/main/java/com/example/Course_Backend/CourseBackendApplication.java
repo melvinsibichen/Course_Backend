185c84ee005d37083fd3d476f6e642f2c17602dd
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-08 19:09:29 +0530

    first

---
+++ src/main/java/com/example/Course_Backend/CourseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Course_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Course_Backend/CourseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Course_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
